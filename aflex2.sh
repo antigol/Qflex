51b43cfa297b7ec3f4b96d37677d6530ebd76f30
Mario Geiger <geiger.mario@gmail.com>
2011-09-30 17:21:49 +0200

    permier commit

---
+++ aflex2.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# fichier bash
+
+wget "http://...."
+
+
+progenc "fichier.xml" > sortie.bash
+
+bash sortie.bash
+
---
+++ progc.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        return 1;
+    }
+    
+    int file = fopen(argv[1]);
+    seekf(file, -1);
+    int end = ...
+
+    char ligne[100];
+
+    while (file fini) {
+        ligne = lirelignedufichier
+        
+        parse la ligne
+        
+        printf("wget %s", lechemin);
+    }
+    //
+    
+    return 0;
+}
+
