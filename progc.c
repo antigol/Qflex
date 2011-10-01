4db2f28eaa736b5e41baa0f2db19348ba4c33af7
Mario Geiger <geiger.mario@gmail.com>
2011-10-01 19:03:29 +0200

    c program

--- progc.c
+++ progc.c
@@ -1,26 +1,55 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
+
+#define BUFFER_SIZE 100
 
 int main(int argc, char *argv[])
 {
     if (argc < 2) {
+        perror ("Error arguments");
         return 1;
     }
     
-    int file = fopen(argv[1]);
-    seekf(file, -1);
-    int end = ...
-
-    char ligne[100];
-
-    while (file fini) {
-        ligne = lirelignedufichier
-        
-        parse la ligne
-        
-        printf("wget %s", lechemin);
+    FILE *file = fopen(argv[1], "r");
+    
+    if (file == NULL) {
+        perror ("Error opening file");
+        return 1;
     }
-    //
+
+    int i = 0;
+
+    char line[BUFFER_SIZE];
+
+    printf("# /bin/bash \n");
+
+    while (!feof(file)) {
+        if (fgets(line, BUFFER_SIZE, file) == NULL) {
+            return 0;
+        }
+        
+
+        char *begpath = strstr(line, "<link>");
+        if (begpath == NULL) {
+            continue;
+        }
+        
+        begpath += 6; // strlen("<link>");
+
+        
+        char *endpath = strstr(begpath, "</link>");
+        if (endpath == NULL) {
+            continue;
+        }
+        
+        endpath[0] = '\0';
+        
+        
+        printf("wget %s\n", begpath);
+    }
+    
+    fclose(file);
     
     return 0;
 }
